d0c1a482ff7fc73a802210a230428ca8f2df1207
kiryanchi <kiryanchi@gmail.com>
2024-09-22 01:24:16 +0900

    Feat: extractUserId from accessor

--- src/main/java/team/gokiyeonmin/imacheater/domain/chat/controller/ChatController.java
+++ src/main/java/team/gokiyeonmin/imacheater/domain/chat/controller/ChatController.java
@@ -1,13 +1,19 @@
 package team.gokiyeonmin.imacheater.domain.chat.controller;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.RestController;
 import team.gokiyeonmin.imacheater.domain.chat.dto.req.ChatMessageSendRequest;
 import team.gokiyeonmin.imacheater.domain.chat.service.ChatMessageService;
+import team.gokiyeonmin.imacheater.global.security.domain.CustomUserDetail;
+
+import java.util.Objects;
 
 @Slf4j
 @RestController
@@ -19,9 +25,16 @@ public class ChatController {
     @MessageMapping("/chat/{roomId}")
     @SendTo("/receive/chat/{roomId}")
     public void sendMessage(
+            @Parameter(hidden = true) StompHeaderAccessor accessor,
             @DestinationVariable Long roomId,
             ChatMessageSendRequest request
     ) {
+        Long userId = getUserId(accessor);
         chatMessageService.createMessage(roomId, request);
+    }
+
+    private Long getUserId(StompHeaderAccessor accessor) {
+        CustomUserDetail user = (CustomUserDetail) ((UsernamePasswordAuthenticationToken) Objects.requireNonNull(accessor.getUser())).getPrincipal();
+        return user.getUserId();
     }
 }
