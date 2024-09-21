c6ecd73741a0b40ce2e52674a78e262fe2647b62
kiryanchi <kiryanchi@gmail.com>
2024-09-22 01:39:46 +0900

    Fix: 채팅 전송 시 유저 아이디 제거

--- src/main/java/team/gokiyeonmin/imacheater/domain/chat/dto/req/ChatMessageSendRequest.java
+++ src/main/java/team/gokiyeonmin/imacheater/domain/chat/dto/req/ChatMessageSendRequest.java
@@ -8,10 +8,6 @@ import jakarta.validation.constraints.NotNull;
 @Schema(description = "채팅 메시지 전송 요청")
 @JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
 public record ChatMessageSendRequest(
-        @Schema(description = "유저 ID", example = "1")
-        @NotNull(message = "유저 ID는 필수입니다.")
-        Long userId,
-
         @Schema(description = "채팅 내용", example = "안녕하세요")
         @NotNull(message = "채팅 내용은 필수입니다.")
         String content
